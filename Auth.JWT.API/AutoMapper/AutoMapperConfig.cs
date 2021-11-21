165eee7e044156ee361f91f0474abe7f71a3952b
Vinicius Bidoia <vinicius.bidoia@hotmail.com>
2021-11-21 19:37:42 -0300

    set AutoMapper Configurations

---
+++ Auth.JWT.API/AutoMapper/AutoMapperConfig.cs
@@ -0,0 +1,20 @@
+﻿using Auth.JWT.API.ViewModel;
+using Auth.JWT.Domain.Models;
+using AutoMapper;
+
+namespace Auth.JWT.API.AutoMapper
+{
+    public class AutoMapperConfig : Profile
+    {
+        public AutoMapperConfig()
+        {
+            /*
+             *  Example
+             *  CreateMap<Fornecedor, FornecedorViewModel>().ReverseMap();
+             */
+
+            //Usuario
+            CreateMap<Usuario, UsuarioViewModel>().ReverseMap();            
+        }
+    }
+}
---
+++ Auth.JWT.API/Controllers/UsuariosValuesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Auth.JWT.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosValuesController : ControllerBase
+    {        
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
